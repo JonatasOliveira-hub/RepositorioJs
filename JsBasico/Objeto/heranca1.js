e81fbfdfa10f0d0fddddfa5472a3eadafaad4db2
Jônatas Oliveira <jonatasoliveira62@yahoo.com.br>
2021-06-11 20:11:45 -0300

    Herança.

---
+++ JsBasico/Objeto/heranca1.js
@@ -0,0 +1,12 @@
+const ferrari = {
+    modelo: 'f40',
+    velMax: 324
+}
+ const volvo = {
+     modelo: 'V40',
+     velMax: 200
+ }
+
+
+
+ console.log(ferrari.__proto__)//Usar dois underline de cada lado para referenciar o objeto pai.
